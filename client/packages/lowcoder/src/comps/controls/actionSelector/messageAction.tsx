bae61fb67d78fd4b648ff56da0dc115bd8e75d2f
moushengkoo <10402885@qq.com>
2024-01-10 11:37:28 +0800

    feat: 全局提示添加通知类型

--- client/packages/lowcoder/src/comps/controls/actionSelector/messageAction.tsx
+++ client/packages/lowcoder/src/comps/controls/actionSelector/messageAction.tsx
@@ -1,13 +1,32 @@
 import { StringControl } from "comps/controls/codeControl";
 import { dropdownControl } from "comps/controls/dropdownControl";
 import { MultiCompBuilder } from "comps/generators/multi";
-import { BranchDiv } from "lowcoder-design";
+import { BranchDiv, notificationInstance } from "lowcoder-design";
 import { trans } from "i18n";
 import { millisecondsControl } from "../millisecondControl";
 import { messageInstance } from "lowcoder-design";
 
 const childrenMap = {
+  messageType: dropdownControl(
+    [
+      { label: trans("message"), value: "message" },
+      { label: trans("notification"), value: "notification" }
+    ] as const,
+    'message'
+  ),
+  placement: dropdownControl(
+    [
+      { "label": trans('top'), "value": "top" },
+      { "label": trans('bottom'), "value": "bottom" },
+      { "label": trans('topLeft'), "value": "topLeft" },
+      { "label": trans('topRight'), "value": "topRight" },
+      { "label": trans('bottomLeft'), "value": "bottomLeft" },
+      { "label": trans('bottomRight'), "value": "bottomRight" },
+    ] as const,
+    'top'
+  ),
   text: StringControl,
+  description: StringControl,
   level: dropdownControl(
     [
       { label: trans("information"), value: "info" },
@@ -22,21 +41,48 @@ const childrenMap = {
 
 export const MessageAction = new MultiCompBuilder(
   childrenMap,
-  (props) => () => messageInstance[props.level](props.text, props.duration / 1000)
+  (props) => () => {
+    if (props.messageType === 'message')
+      messageInstance[props.level](props.text, props.duration / 1000)
+    else {
+      notificationInstance[props.level]({
+        message: props.text,
+        description: props.description,
+        placement: props.placement,
+        duration: props.duration / 1000,
+      })
+    }
+  }
 )
   .setPropertyViewFn((children) => (
     <>
+      <BranchDiv>
+        {children.messageType.propertyView({
+          label: trans("eventHandler.messageType"),
+        })}
+      </BranchDiv>
       <BranchDiv>
         {children.text.propertyView({
           label: trans("eventHandler.text"),
           layout: "vertical",
         })}
       </BranchDiv>
+      {children.messageType.getView() === "notification" && <BranchDiv>
+        {children.description.propertyView({
+          label: trans("eventHandler.description"),
+          layout: "vertical",
+        })}
+      </BranchDiv>}
       <BranchDiv $type={"inline"}>
         {children.level.propertyView({
           label: trans("eventHandler.level"),
         })}
       </BranchDiv>
+      {children.messageType.getView() === "notification" && <BranchDiv $type={"inline"}>
+        {children.placement.propertyView({
+          label: trans("eventHandler.placement"),
+        })}
+      </BranchDiv>}
       <BranchDiv>
         {children.duration.propertyView({
           label: trans("eventHandler.duration"),
